3620dad4c62c6d98ccec6faddfdc61c0a12a05a0
exaul <edwin.falcones@tecnologicoliceocristiano.edu.ec>
2023-11-14 20:28:30 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TecnologicoApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TecnologicoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
