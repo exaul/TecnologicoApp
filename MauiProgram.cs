ea572a538ceb291eded83be7797d0cccd54ff247
exaul <edwin.falcones@tecnologicoliceocristiano.edu.ec>
2023-11-18 18:02:58 -0500

    tarea

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace TecnologicoApp
 {
@@ -9,6 +10,7 @@ namespace TecnologicoApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,9 +18,8 @@ namespace TecnologicoApp
                 });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
